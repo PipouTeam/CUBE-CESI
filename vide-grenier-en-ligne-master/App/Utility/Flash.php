e1ce97b4410e4da0ad7afcf8723ea4009dd0599b
Anaderis <anais.kajjaj@outlook.fr>
2025-04-25 16:08:34 +0200

    Adding test for danger and getError functions in Flash

--- vide-grenier-en-ligne-master/App/Utility/Flash.php
+++ vide-grenier-en-ligne-master/App/Utility/Flash.php
@@ -3,10 +3,13 @@
 namespace App\Utility;
 
 class Flash {
+
+    //Danger() stocke le message d'erreur dans la session
     public static function danger($message) {
         $_SESSION['flash_error'] = $message;
     }
 
+    //getError affiche le message d'erreur stocké dans une page html, puis le supprime de la session
     public static function getError() {
         if (isset($_SESSION['flash_error'])) {
             $msg = $_SESSION['flash_error'];
