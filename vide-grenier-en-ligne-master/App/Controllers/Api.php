36ef3abcef4db2d878697d364ac8bb40f8000513
Raspy <enora.broudic@clever-cloud.com>
2025-04-30 14:04:44 +0200

    Add city information display to products on home page and detail page

--- vide-grenier-en-ligne-master/App/Controllers/Api.php
+++ vide-grenier-en-ligne-master/App/Controllers/Api.php
@@ -23,6 +23,16 @@ class Api extends \Core\Controller
         $query = $_GET['sort'];
 
         $articles = Articles::getAll($query);
+        
+        // Add city information to each article
+        foreach ($articles as &$article) {
+            if (isset($article['ville_id'])) {
+                $city = Cities::getById($article['ville_id']);
+                if ($city) {
+                    $article['city'] = $city;
+                }
+            }
+        }
 
         header('Content-Type: application/json');
         echo json_encode($articles);
@@ -39,5 +49,30 @@ class Api extends \Core\Controller
 
         header('Content-Type: application/json');
         echo json_encode($cities);
+    }
+    
+    /**
+     * Récupère les informations d'une ville par son ID
+     *
+     * @throws Exception
+     */
+    public function CityAction(){
+        if (!isset($_GET['id'])) {
+            header('HTTP/1.1 400 Bad Request');
+            echo json_encode(['error' => 'Missing city ID']);
+            return;
+        }
+        
+        $cityId = (int)$_GET['id'];
+        $city = Cities::getById($cityId);
+        
+        if (!$city) {
+            header('HTTP/1.1 404 Not Found');
+            echo json_encode(['error' => 'City not found']);
+            return;
+        }
+        
+        header('Content-Type: application/json');
+        echo json_encode($city);
     }
 }
