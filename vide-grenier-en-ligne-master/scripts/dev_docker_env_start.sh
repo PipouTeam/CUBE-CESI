7d05dd5e450b9b8e7ac9e87589f0d9786db893a7
Anaderis <anais.kajjaj@outlook.fr>
2025-05-22 17:14:54 +0200

    ajout du chmod sur storage

--- vide-grenier-en-ligne-master/scripts/dev_docker_env_start.sh
+++ vide-grenier-en-ligne-master/scripts/dev_docker_env_start.sh
@@ -6,4 +6,6 @@ docker compose -f docker/docker-compose.stage.yml --project-name stage down
 docker compose -f docker/docker-compose.prod.yml --project-name prod down
 
 echo "Starting development environment..."
+# -R effectue l'action sur tous les fichiers présentes
+chmod -R 777 public/storage
 docker compose -f docker/docker-compose.dev.yml --project-name dev up --build -d 
